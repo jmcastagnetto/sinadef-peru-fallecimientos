9920261f4bf5b04411414f8e5f24609b96265e0f
Jesus M. Castagnetto <jesus@castagnetto.com>
2020-06-09 12:16:21 -0500

    actualizado 2020-06-09

--- descargar-datos.sh
+++ descargar-datos.sh
@@ -2,4 +2,4 @@
 
 wget https://cloud.minsa.gob.pe/s/nqF2irNbFomCLaa/download -O datos/fallecidos_sinadef.csv
 iconv -f ISO_8859-1  -t UTF8 datos/fallecidos_sinadef.csv > datos/fallecidos_sinadef-utf8.csv
-gzip -9 datos/fallecidos*csv
+gzip -9f datos/fallecidos*csv
