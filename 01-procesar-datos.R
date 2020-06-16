40fa50d94f7d726aedef709cf38ded609b9960fc
Jesus M. Castagnetto <jesus@castagnetto.com>
2020-06-16 15:18:49 -0500

    procesando los nuevos campos de causas, marcando posibles fallecimientos por COVID-19

--- 01-procesar-datos.R
+++ 01-procesar-datos.R
@@ -1,33 +1,47 @@
 library(tidyverse)
 
+col_spec <- cols(
+  Nº = col_double(),
+  `TIPO SEGURO` = col_character(),
+  SEXO = col_character(),
+  EDAD = col_double(),
+  `TIEMPO EDAD` = col_character(),
+  `ESTADO CIVIL` = col_character(),
+  `NIVEL DE INSTRUCCIÓN` = col_character(),
+  `COD# UBIGEO DOMICILIO` = col_character(),
+  `PAIS DOMICILIO` = col_character(),
+  `DEPARTAMENTO DOMICILIO` = col_character(),
+  `PROVINCIA DOMICILIO` = col_character(),
+  `DISTRITO DOMICILIO` = col_character(),
+  FECHA = col_date(format = ""),
+  AÑO = col_double(),
+  MES = col_double(),
+  `TIPO LUGAR` = col_character(),
+  INSTITUCION = col_character(),
+  `MUERTE VIOLENTA` = col_character(),
+  NECROPSIA = col_character(),
+  `DEBIDO A (CAUSA A)` = col_character(),
+  `CAUSA A (CIE-X)` = col_character(),
+  `DEBIDO A (CAUSA B)` = col_character(),
+  `CAUSA B (CIE-X)` = col_character(),
+  `DEBIDO A (CAUSA C)` = col_character(),
+  `CAUSA C (CIE-X)` = col_character(),
+  `DEBIDO A (CAUSA D)` = col_character(),
+  `CAUSA D (CIE-X)` = col_character(),
+  `DEBIDO A (CAUSA E)` = col_character(),
+  `CAUSA E (CIE-X)` = col_character(),
+  `DEBIDO A (CAUSA F)` = col_character(),
+  `CAUSA F (CIE-X)` = col_character()
+)
+
 sinadef_raw <- read_csv2(
   "datos/fallecidos_sinadef-utf8.csv.gz",
   skip = 2,
-  col_types = cols(
-    Nº = col_integer(),
-    `TIPO SEGURO` = col_character(),
-    SEXO = col_character(),
-    EDAD = col_number(),
-    `TIEMPO EDAD` = col_character(),
-    `ESTADO CIVIL` = col_character(),
-    `NIVEL DE INSTRUCCIÓN` = col_character(),
-    `COD# UBIGEO DOMICILIO` = col_character(),
-    `PAIS DOMICILIO` = col_character(),
-    `DEPARTAMENTO DOMICILIO` = col_character(),
-    `PROVINCIA DOMICILIO` = col_character(),
-    `DISTRITO DOMICILIO` = col_character(),
-    FECHA = col_date(),
-    AÑO = col_integer(),
-    MES = col_integer(),
-    `TIPO LUGAR` = col_character(),
-    INSTITUCION = col_character(),
-    `MUERTE VIOLENTA` = col_character(),
-    NECROPSIA = col_character()
-  ),
+  col_types = col_spec,
   na = c("", "SIN REGISTRO", "NA")
 ) %>%
   janitor::clean_names() %>%
-  select(1:19)
+  select(-c(32:35))
 
 estciv_mayores <- c("CASADO", "DIVORCIADO", "SEPARADO",
                     "VIUDO", "CONVIVIENT/CONCUBINA")
@@ -75,19 +89,55 @@ sinadef_df <- sinadef_raw %>%
       str_replace("estados unidos de america", "USA"),
     iso3c = countrycode::countryname(pais_en, destination = "iso3c")
   ) %>%
-  mutate_at(
-    vars(tipo_seguro, sexo,
-         tiempo_edad, tiempo_edad_orig,
-         estado_civil,
-         nivel_de_instruccion,
-         cod, number, dept, prov, dist, domicilio,
-         pais_domicilio, departamento_domicilio,
-         distrito_domicilio,
-         pais_en, iso3c,
-         tipo_lugar, institucion, muerte_violenta,
-         necropsia),
-    factor
+  mutate_if(
+    is.character,
+    str_trim
+  ) %>%
+  mutate_if(
+    is.character,
+    str_squish
+  ) %>%
+  mutate(
+    covid19_a = str_detect(debido_a_causa_a,
+                          "(CORONAVIRUS|COVID|SARS COV|SARS-COV)") %>%
+      replace_na(FALSE),
+    covid19_b = str_detect(debido_a_causa_b,
+                          "(CORONAVIRUS|COVID|SARS COV|SARS-COV)") %>%
+      replace_na(FALSE),
+    covid19_c = str_detect(debido_a_causa_c,
+                          "(CORONAVIRUS|COVID|SARS COV|SARS-COV)") %>%
+      replace_na(FALSE),
+    covid19_d = str_detect(debido_a_causa_d,
+                          "(CORONAVIRUS|COVID|SARS COV|SARS-COV)") %>%
+      replace_na(FALSE),
+    covid19_e = str_detect(debido_a_causa_e,
+                          "(CORONAVIRUS|COVID|SARS COV|SARS-COV)") %>%
+      replace_na(FALSE),
+    covid19_f = str_detect(debido_a_causa_f,
+                          "(CORONAVIRUS|COVID|SARS COV|SARS-COV)") %>%
+      replace_na(FALSE)
+  ) %>%
+  rowwise() %>%
+  mutate(
+    covid19_causa = (covid19_a | covid19_b | covid19_c | covid19_d | covid19_e | covid19_f),
+    covid19_n_causa = sum(covid19_a, covid19_b, covid19_c, covid19_d, covid19_e, covid19_f, na.rm = TRUE)
   )
+
+all_causes <- bind_rows(
+  sinadef_raw %>% select(codigo_ciex = causa_a_cie_x, causa = debido_a_causa_a),
+  sinadef_raw %>% select(codigo_ciex = causa_b_cie_x, causa = debido_a_causa_b),
+  sinadef_raw %>% select(codigo_ciex = causa_c_cie_x, causa = debido_a_causa_c),
+  sinadef_raw %>% select(codigo_ciex = causa_d_cie_x, causa = debido_a_causa_d),
+  sinadef_raw %>% select(codigo_ciex = causa_e_cie_x, causa = debido_a_causa_e),
+  sinadef_raw %>% select(codigo_ciex = causa_f_cie_x, causa = debido_a_causa_f)
+) %>%
+  arrange(codigo_ciex, causa) %>%
+  distinct()
+
+write_csv(
+  all_causes,
+  path = "datos/causas_reportadas_sinadef.csv"
+)
 
 write_csv(
   sinadef_df,
--- README.md
+++ README.md
@@ -9,3 +9,6 @@ Análisis usando los datos de
 - **2020-06-08**:
   - Hoy el formato de fecha a cambiado de "DD/MM/YYYY" a "YYYY-MM-DD"
   - Los CSV ahora se guardarán comprimidos para no llegar a los límites de github
+
+- **2020-06-16**:
+  - Con la adición de campos de causas, estoy calculando campos que marcan si las causas contienen la expresión regular: `"(CORONAVIRUS|COVID|SARS COV|SARS-COV)"` se encuentra presente, y marcar como posible registro de fallecimiento debido a COVID-19.
